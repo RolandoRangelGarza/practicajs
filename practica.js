bbc1a1590c2cbb1b4b6e7e95b44a5229736953e4
RolandoRangelGarza <rolando.rangelgr@uanl.edu.mx>
2022-10-12 10:29:22 -0500

    base para programa

--- practica.js
+++ practica.js
@@ -1,7 +1,23 @@
 /* Variable hora de la toma inicial */
-let h_tomainicial = 0
+let h_tomainicial = 0 //0-23
 
 /* Variable frecuencia de la toma diaria */
-let f_tomadiaria = 0
+let f_tomadiaria = 0 //1-12
+
+/* Verificar datos */
+console.log(`Hora inicial de la toma: ${h_tomainicial}`);
+console.log(`Frecuencia: ${h_tomainicial}`);
+
+let suma = 0
+for( let i=1; i<=f_tomadiaria; i++) {
+    let h_tomainicial = h_tomainicial + suma;
+    let codigo = "<p>" + `toma` + i + `` + h_tomainicial+ "</p>";
+    document.write(codigo);
+}
+
+
+console.log("Horarios");
+console.log(`Toma ${contador} ${hora_contador}`);
+
 
 
