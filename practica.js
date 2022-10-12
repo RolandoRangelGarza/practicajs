a9c1da998b1a1be60e3aef899892370c38ac6b9e
RolandoRangelGarza <rolando.rangelgr@uanl.edu.mx>
2022-10-12 10:42:56 -0500

    base ciclo sin validaciones y html

--- practica.js
+++ practica.js
@@ -1,20 +1,20 @@
 /* Variable hora de la toma inicial */
-let h_tomainicial = 0 //0-23
+let h_tomainicial = 2 //0-23
 
 /* Variable frecuencia de la toma diaria */
-let f_tomadiaria = 0 //1-12
+let f_tomadiaria = 3 //1-12
 
 /* Verificar datos */
 console.log(`Hora inicial de la toma: ${h_tomainicial}`);
 console.log(`Frecuencia: ${h_tomainicial}`);
 
-let suma = 0
-for( let i=1; i<=f_tomadiaria; i++) {
-    let h_tomainicial = h_tomainicial + suma;
-    let codigo = "<p>" + `toma` + i + `` + h_tomainicial+ "</p>";
-    document.write(codigo);
-}
+let contador = 1
 
+while ( contador <= f_tomadiaria ) {
+    document.write("<p>" + "toma" + "&nbsp;" + contador + "&nbsp;" + h_tomainicial + "&nbsp;" + "</p>");
+    contador ++;
+    h_tomainicial += h_tomainicial;
+}
 
 console.log("Horarios");
 console.log(`Toma ${contador} ${hora_contador}`);
